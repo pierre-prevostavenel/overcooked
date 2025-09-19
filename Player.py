2a0eb884c4b3afa2bbb82e6da223ded93f4a4faa
Pierre Prevost--Avenel <103773957+pierre-prevostavenel@users.noreply.github.com>
2025-09-19 16:06:54 +0200

    Create Player.py

---
+++ Player.py
@@ -0,0 +1,45 @@
+import pygame
+import random
+
+class Player:
+    def __init__(self, x=0, y=0):
+        self.largeur_map = 10
+        self.position = x * self.largeur_map + y
+
+    def update(self, keys):
+        # Si le joueur est contrôlé par les touches
+        if True:
+            if keys[pygame.K_z] and self.position >= self.largeur_map:
+                self.position -= self.largeur_map
+
+            if keys[pygame.K_s] and self.position < self.largeur_map * (self.largeur_map - 1):
+                self.position += self.largeur_map
+
+            if keys[pygame.K_q] and self.position % self.largeur_map != 0:
+                self.position -= 1
+
+            if keys[pygame.K_d] and self.position % self.largeur_map != self.largeur_map - 1:
+                self.position += 1
+        else:
+            mouvements_possibles = []
+            
+            # haut
+            if self.position >= self.largeur_map:
+                mouvements_possibles.append(-self.largeur_map)
+
+            # bas
+            if self.position < self.largeur_map * (self.largeur_map - 1):
+                mouvements_possibles.append(self.largeur_map)
+
+            # gauche
+            if self.position % self.largeur_map != 0:
+                mouvements_possibles.append(-1)
+
+            # droite
+            if self.position % self.largeur_map != self.largeur_map - 1:
+                mouvements_possibles.append(1)
+
+            # On choisit un mouvement uniquement parmi ceux qui sont valides
+            if mouvements_possibles:
+                mouvement = random.choice(mouvements_possibles)
+                self.position += mouvement
