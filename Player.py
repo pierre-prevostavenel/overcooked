22b995818f4bc7f7b04ec840d226577553edbaf8
pierre.prevost-avenel@etu.umontpellier.fr <pierre.prevost-avenel@etu.umontpellier.fr>
2025-09-19 16:40:26 +0200

    Updates player

--- Player.py
+++ Player.py
@@ -3,46 +3,47 @@ import random
 
 class Player:
     def __init__(self, x=0, y=0):
-        self.largeur_map = 10
-        self.position = x * self.largeur_map + y
+        self.map_width = 10
+        self.position = x * self.map_width + y
+        self.manual_control = False
 
-    def update(self, manual_control=True):
+    def update(self):
         # Si le joueur est contrôlé par les touches
-        if manual_control:
+        if self.manual_control:
             keys = pygame.key.get_pressed()
 
-            if keys[pygame.K_z] and self.position >= self.largeur_map:
-                self.position -= self.largeur_map
+            if keys[pygame.K_z] and self.position >= self.map_width:
+                self.position -= self.map_width
 
-            if keys[pygame.K_s] and self.position < self.largeur_map * (self.largeur_map - 1):
-                self.position += self.largeur_map
+            if keys[pygame.K_s] and self.position < self.map_width * (self.map_width - 1):
+                self.position += self.map_width
 
-            if keys[pygame.K_q] and self.position % self.largeur_map != 0:
+            if keys[pygame.K_q] and self.position % self.map_width != 0:
                 self.position -= 1
 
-            if keys[pygame.K_d] and self.position % self.largeur_map != self.largeur_map - 1:
+            if keys[pygame.K_d] and self.position % self.map_width != self.map_width - 1:
                 self.position += 1
         else:
             mouvements_possibles = []
             
             # haut
-            if self.position >= self.largeur_map:
-                mouvements_possibles.append(-self.largeur_map)
+            if self.position >= self.map_width:
+                mouvements_possibles.append(-self.map_width)
 
             # bas
-            if self.position < self.largeur_map * (self.largeur_map - 1):
-                mouvements_possibles.append(self.largeur_map)
+            if self.position < self.map_width * (self.map_width - 1):
+                mouvements_possibles.append(self.map_width)
 
             # gauche
-            if self.position % self.largeur_map != 0:
+            if self.position % self.map_width != 0:
                 mouvements_possibles.append(-1)
 
             # droite
-            if self.position % self.largeur_map != self.largeur_map - 1:
+            if self.position % self.map_width != self.map_width - 1:
                 mouvements_possibles.append(1)
 
             # On choisit un mouvement uniquement parmi ceux qui sont valides
             if mouvements_possibles:
                 mouvement = random.choice(mouvements_possibles)
                 self.position += mouvement
-
+        print(f"Position du joueur : {self.position%self.map_width}, {self.position // self.map_width}")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame.sprite import LayeredUpdates, Sprite
 from MySprite import MySprite
+from Player import Player
 
 pygame.init()
 screen_width = 1920
 screen_height = 1080
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+player = Player()
 
 all_sprites = LayeredUpdates()
 running = True
@@ -14,11 +16,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # On inverse la valeur de manual_control
+                player.manual_control = not player.manual_control
 
     all_sprites.update()
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)  # Order is based on layer
     pygame.display.flip()
+    player.update()
     clock.tick(60)
 
 pygame.quit()
