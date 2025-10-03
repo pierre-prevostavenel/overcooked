992d4da661ee735e7066fc397137a45c06f6ba79
Wissam El Hage <elhagewissi@gmail.com>
2025-10-03 14:32:46 +0200

    changed ingredients_list.py

--- ingredients_list.py
+++ ingredients_list.py
@@ -1,15 +1,30 @@
-from Ingredient import Ingredient  
+from Ingredient import Ingredient
 
+# Ingrédients
 fries = Ingredient("Fries")
 cooked_potato = Ingredient("Cooked Potato")
-chopped_potato = Ingredient("Chopped Potato", next_stages=[fries])
-potato = Ingredient("Potato", next_stages=[chopped_potato, cooked_potato])
+chopped_potato = Ingredient("Chopped Potato")
+potato = Ingredient("Potato")
 
 chopped_tomato = Ingredient("Chopped Tomato")
-tomato = Ingredient("Tomato", next_stages=[chopped_tomato])
+tomato = Ingredient("Tomato")
 
 chopped_lettuce = Ingredient("Chopped Lettuce")
-lettuce = Ingredient("Lettuce", next_stages=[chopped_lettuce])
+lettuce = Ingredient("Lettuce")
 
 steak_cooked = Ingredient("Cooked Steak")
-steak_raw = Ingredient("Steak", next_stages=[steak_cooked])
+steak_raw = Ingredient("Steak")
+
+# Liaisons fixes (prédefinies)
+potato.evolutions = {"chop": chopped_potato, "cook": cooked_potato}
+chopped_potato.evolutions = {"fry": fries}
+
+tomato.evolutions = {"chop": chopped_tomato}
+lettuce.evolutions = {"chop": chopped_lettuce}
+
+steak_raw.evolutions = {"cook": steak_cooked}
+
+# Création des liens précédents
+for parent in [potato, chopped_potato, tomato, lettuce, steak_raw]:
+    for action, child in parent.evolutions.items():
+        child.previous = (parent, action)
